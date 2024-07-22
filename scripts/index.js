bee4ee3112dfbfbf1bcc2399141b71b036a66560
ytka98 <rickda1ton@yandex.ru>
2024-07-22 21:12:57 +0300

    Added JS functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+console.log(cardTemplate);
 
 // @todo: DOM узлы
+const cardWrapper = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+const generateCard = (name, link, eventCallback) => {
+  const cardElement = cardTemplate.cloneNode(true).querySelector(".card");
+  // Находим нужные элементы внутри клонированного шаблона
+  const image = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const title = cardElement.querySelector(".card__title");
+  // Устанавливаем текст и атрибуты элементов
+  title.textContent = name;
+  image.alt = name;
+  image.src = link;
+  // Добавляем обработчик события для кнопки удаления
+  deleteButton.addEventListener("click", eventCallback);
+  // Возвращаем созданный элемент карточки
+  return cardElement;
+};
 
 // @todo: Функция удаления карточки
-
+const terminateCard = (evt) => {
+  evt.target.closest(".card").remove();
+};
 // @todo: Вывести карточки на страницу
+
+// Вывести карточки на страницу
+initialCards.forEach((item) => {
+  cardWrapper.append(generateCard(item.name, item.link, terminateCard));
+});
